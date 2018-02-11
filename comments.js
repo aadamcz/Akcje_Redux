d6db9b0003b622bcb5ad37487cbd24d9f5bafd5b
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-02-11 16:37:47 +0100

    Format comments.js

--- comments.js
+++ comments.js
@@ -1,12 +1,18 @@
+import (ADD_COMMENT) from './actions';
+import (REMOVE_COMMENT) from './actions';
+import (EDIT_COMMENT) from './actions';
+import (THUMB_UP_COMMENT) from './actions';
+import (THUMB_DOWN_COMMENT) from './actions';
+
 function comments(state = [], action) { //początkowy stan aplikacji to pusta tablica
 	switch (action.type) {
 		case ADD_COMMENT:
 			return [{
-						id: action.id,
-						text: action.text,
-						votes: 0
-					},
-					...state.comments];	
+				id: action.id,
+				text: action.text,
+				votes: 0
+				},
+				...state.comments];	
 		case REMOVE_COMMENT:
 			return  state.comments.filter(comment => comment.id !== action.id);	
 		case EDIT_COMMENT:
