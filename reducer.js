25bbe1ac990cf914c55ab1facf0242ba7be92bec
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-02-11 16:36:12 +0100

    Modify reducers.js, create comments.js

--- reducer.js
+++ reducer.js
@@ -1,8 +1,3 @@
-import (ADD_COMMENT) from './actions';
-import (REMOVE_COMMENT) from './actions';
-import (EDIT_COMMENT) from './actions';
-import (THUMB_UP_COMMENT) from './actions';
-import (THUMB_DOWN_COMMENT) from './actions';
 import {combineReducers} from 'redux';
 import comments from './comments';
 
